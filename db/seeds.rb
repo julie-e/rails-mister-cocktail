ba8d2288c8a884510bb86694d4e29c11cde10b19
Julie Ehrmann <julie.ehrmann1@gmail.com>
2017-02-09 16:30:56 +0100

    Updated my database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
 
-i1 = Ingredient.create(name: "lemon")
-puts "#{i1} was created"
+Cocktail.destroy_all
+Ingredient.destroy_all
 
-i2 = Ingredient.create(name: "ice")
-puts "#{i2} was created"
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredients = JSON.parse(ingredient_serialized)
 
-i3= Ingredient.create(name: "mint leaves")
-puts "#{i3} was created"
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create!(name: ingredient.values.first)
+  puts "#{i} was created !"
+end
 
-i4= Ingredient.create(name: "cucumber")
-puts "#{i4} was created"
 
-i5= Ingredient.create(name: "strawberry")
-puts "#{i5} was created"
 
-i6= Ingredient.create(name: "coke")
-puts "#{i6} was created"
 
-i7= Ingredient.create(name: "tonic")
-puts "#{i7} was created"
+c1 = Cocktail.create!(name: "Mojito")
+puts "#{c1} was created"
 
-i8= Ingredient.create(name: "passion fruit")
-puts "#{i8} was created"
+c2 = Cocktail.create!(name: "Caipirinha")
+puts "#{c2} was created"
 
-i9= Ingredient.create(name: "aperol")
-puts "#{i9} was created"
+c3= Cocktail.create!(name: "Spritz")
+puts "#{c3} was created"
 
-i10= Ingredient.create(name: "sparkling water")
-puts "#{i10} was created"
+c4= Cocktail.create!(name: "Gin Tonic")
+puts "#{c4} was created"
+
+c5= Cocktail.create!(name: "Long Island")
+puts "#{c5} was created"
