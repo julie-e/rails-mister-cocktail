fa8ee8651d04a5e1e08c2778ec0558dfae92e536
Julie Ehrmann <julie.ehrmann1@gmail.com>
2017-02-13 07:37:52 +0100

    Added som emore CSS (search bar and clickable cards

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,6 +3,11 @@ class CocktailsController < ApplicationController
 
   def index
     @cocktails = Cocktail.all
+    if params[:search]
+      @cocktails = Cocktail.search(params[:search])
+    else
+      @cocktails = Cocktail.all
+    end
   end
 
   def show
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,9 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   validates :name, presence: true, allow_nil: false, uniqueness:true;
   mount_uploader :photo, PhotoUploader
+
+
+  def self.search(search)
+    where("name ILIKE ?", "%#{search}%")
+  end
 end
