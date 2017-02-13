30509b25d1c1b0cd14a32b350370132a2a4896b5
Julie Ehrmann <julie.ehrmann1@gmail.com>
2017-02-13 07:45:11 +0100

    Added the new route for Doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,6 +1,10 @@
 class DosesController < ApplicationController
   before_action :set_cocktail, only: [:create]
 
+  def new
+    @dose = Dose.new
+  end
+
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
